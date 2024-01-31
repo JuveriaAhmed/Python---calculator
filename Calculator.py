74ae79c13de97c09a05656d84454d36443e2c8e6
JuveriaAhmed <152685766+JuveriaAhmed@users.noreply.github.com>
2024-01-31 16:40:15 +0500

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,247 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# BMI Calculator
+
+# In[11]:
+
+
+name= input(" Enter your name: ")
+weight = int(input("Enter your weight in pounds: "))
+height = int(input("Enter your inches in inches: "))
+BMI = (weight * 703) / (height  * height )
+print(BMI)
+
+if BMI >0:
+    if(BMI < 18.5):
+        print(name, "You are under weight")
+    elif (BMI <= 24.9):
+        print (name,"You are normal weight")
+    elif (BMI < 29.9):
+        print(name, "You are over weight")
+    elif (BMI < 34.9):
+        print(name, "you are Obease")    
+    elif (BMI < 39.9):
+        print(name, "You are severly obease")        
+    elif (BMI > 39.9):
+        print(name, "You are morbidly obease")
+    else:
+            print ("Enter valid inputs")
+
+
+# In[10]:
+
+
+
+
+
+# In[7]:
+
+
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#BMI = (weight in pounds x 703) / (height in inches x height in inches)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
